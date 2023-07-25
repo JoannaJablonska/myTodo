5ecab60019642169ffa84099bd2a81e490fa0283
Łukasz <lukasz.slazyk@lhsystems.com>
2023-07-25 14:14:40 +0200

    Initial

---
+++ src/test/java/com/lhg/mytodo/MyTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhg.mytodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
